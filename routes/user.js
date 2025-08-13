274e6913c7447577cf59045a44610caa06ef452b
DarkDanny25 <edanuc15@gmail.com>
2025-08-13 00:33:04 -0500

    agregando backend v3

--- routes/user.js
+++ routes/user.js
@@ -70,7 +70,7 @@ router.post('/login', validateLogin, handleValidation, async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'Lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       maxAge: 24 * 60 * 60 * 1000,
       path: '/',
     });
