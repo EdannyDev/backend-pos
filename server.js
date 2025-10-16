fb72f0567a04dd4e777bfcce284647dfd7340a66
DarkDanny25 <edanuc15@gmail.com>
2025-10-15 20:00:32 -0500

    cambios en producción

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [ "http://localhost:3000", "https://pos-app-green.vercel.app" ],
   credentials: true,
 }));
 app.use(express.json());
