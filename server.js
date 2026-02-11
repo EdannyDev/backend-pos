a721f1dbd5683d3e941f7dc6f469501ecf3fa8d0
Danny <edanuc15@gmail.com>
2026-02-10 21:10:56 -0500

    ajustes de producción

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: [ "http://localhost:3000"],
+  origin: [ "http://localhost:3000", "https://techno-pos.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
